232f4cd2a328c87ff35658252c3e7099f0817c85
FanZhong Zeng <fanzhongzeng78@gmail.com>
2021-12-13 12:43:33 -0500

    week1 2/2 done

--- Week1/StaircaseOfRecursion.java
+++ Week1/StaircaseOfRecursion.java
@@ -5,8 +5,17 @@ public class MyClass {
         System.out.println(waysToClimb(5));
     }
     
+    //This is basically fibonacci sequence
     public static int waysToClimb(int n){
-
-        return -1
+        //System.out.println("n: " + n);
+        
+        if (n == 1 || n == 0)
+            return 1;
+        else
+        {
+            return waysToClimb(n-1) + waysToClimb(n-2); 
+        }        
     }
+
+    
 }
