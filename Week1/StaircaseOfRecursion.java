054eaee36fb0b1fa2cf064ef46a7c2124d701ad8
FanZhong Zeng <fanzhongzeng78@gmail.com>
2021-12-13 13:17:40 -0500

    refined waysToClimb function

--- Week1/StaircaseOfRecursion.java
+++ Week1/StaircaseOfRecursion.java
@@ -3,6 +3,10 @@ public class MyClass {
         System.out.println(waysToClimb(1) + "\n");
         System.out.println(waysToClimb(2)+ "\n");
         System.out.println(waysToClimb(5));
+
+        System.out.println(waysToClimb2(1) + "\n");
+        System.out.println(waysToClimb2(2)+ "\n");
+        System.out.println(waysToClimb2(5));
     }
     
     //This is basically fibonacci sequence
@@ -17,5 +21,37 @@ public class MyClass {
         }        
     }
 
-    
+    //Learning Dynamic Programming
+    //Concept is doing it bottom up, starting from f(0) and f(1) and keep on going until f(n)
+    //Since Fibonacci sequence only needs the previous 2 number
+    public static int waysToClimb2(int n) {
+        //If n is 0 or 1, just return 1, no need for calculation
+        if (n == 0 || n == 1)
+            return 1;
+        
+        //def the first two number, fibonacci first 2 number is always 1;
+        int num1 = 1;
+        int num2 = 1;
+        
+        /*for (int i = 2; i <= n; i++)
+        {
+            //Getting the result for current i
+            int sum = num1 + num2;
+            //System.out.println("i: " + i + " | Sum: " + sum);
+            
+            //Replacing the two numbers
+            num1 = num2;
+            num2 = sum;
+        }*/
+        
+        //Trying to replace numbers without creating new integer
+        for (int i = 2; i <= n; i++)
+        {
+            num2 += num1;
+            num1 = num2 - num1;
+        }
+        
+        //num2 is the latest result so we return that.
+        return num2;
+    }
 }
