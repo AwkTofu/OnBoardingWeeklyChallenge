f6c5a3c867e80cb248d1fead53719f3f73bbb40f
FanZhong Zeng <fanzhongzeng78@gmail.com>
2021-12-13 12:23:15 -0500

    added week1 md

---
+++ Week1/StaircaseOfRecursion.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+        System.out.println(waysToClimb(1) + "\n");
+        System.out.println(waysToClimb(2)+ "\n");
+        System.out.println(waysToClimb(5));
+    }
+    
+    public static int waysToClimb(int n){
+
+        return -1
+    }
+}
